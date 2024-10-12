c3b54bb37613619e016e718e7fba61c87baf9584
juan.sossa <juan.sossaQcun.edu.co>
2024-10-11 22:02:19 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RickAndMortyService } from './service/rick-and-morty.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rick-and-morty-app';
+export class AppComponent implements OnInit {
+  characters: any[] = [];
+
+  constructor(private rickAndMortyService: RickAndMortyService) {}
+
+  ngOnInit() {
+    this.rickAndMortyService.getCharacters().subscribe((data) => {
+      this.characters = data.results; 
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';  // Añadir esto
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule  // Añadir esto
   ],
   providers: [],
   bootstrap: [AppComponent]
